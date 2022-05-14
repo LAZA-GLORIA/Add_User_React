4ae5c84a7d6795bbb273e64cdd1094600d2d45ed
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-05-14 13:08:32 +0200

    useReducer dans un composant de type fonction ex composant Count

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,15 +1,25 @@
 import React, {useReducer} from "react"
 
   //Les initialiser à l'extérieur de la fonction Count
-  const initialState = 0;
+  //const initialState = 0;
+  //Le state initial est différent du state, le state a la valeur du 
+  //compteur actuel
+  const initialState = {
+      countOne: 0,
+      countTwo: 0
+  }
 
   //Je lui le state et l'action pour obtenir le nouveau state
   const reducer = (state, action) => {
-      switch(action) {
+      switch(action.type) {
           case "increment":
-              return state + 1
+              return {...state, countOne: state.countOne + action.value}
           case "decrement":
-              return state - 1
+              return {...state, countOne: state.countOne - action.value}
+          case "increment2":
+                return {...state, countTwo: state.countTwo + action.value}
+          case "decrement2":
+                return {...state, countTwo: state.countTwo - action.value}
           case "reinitialiser":
               return initialState
           default:
@@ -25,12 +35,22 @@ export default function Count() {
     //un state et l'action pour retourner le nouveau state
     const [count, dispatch] = useReducer(reducer, initialState)
     return (
+      <>
         <div>
-            <h1> {count} </h1>
+            <h1> {count.countOne} </h1>
             {/** la methode me permet d'accéder à mon action */}
-            <button onClick={() => dispatch("increment")}> Incrémenter </button>
-            <button onClick={() => dispatch("decrement")}> Decrémenter </button>
-            <button onClick={() => dispatch("reinitialiser")}> Réinitialiser </button>
+            <button onClick={() => dispatch({type: "increment", value: 1})}> Incrémenter </button>
+            <button onClick={() => dispatch({type: "decrement", value:1})}> Decrémenter </button>
+            <button onClick={() => dispatch({type: "reinitialiser"})}> Réinitialiser </button>
         </div>
+
+        <div>
+            <h1> {count.countTwo} </h1>
+            {/** la methode me permet d'accéder à mon action */}
+            <button onClick={() => dispatch({type: "increment2", value: 10})}> Incrémenter </button>
+            <button onClick={() => dispatch({type: "decrement2", value:5})}> Decrémenter </button>
+            <button onClick={() => dispatch({type: "reinitialiser"})}> Réinitialiser </button>
+        </div>
+      </>
     )
 }
